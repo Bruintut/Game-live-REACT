1f3c524fbb22d9b3d1f52f3128c1f441f2f0cc73
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2023-03-15 14:38:47 -0300

    feat: initia config Routers

---
+++ app/src/mocks/products.ts
@@ -0,0 +1,9 @@
+export const products =[
+    {
+        id: '507f191e810c19729de86ea',
+        name: 'Anchovas',
+        description: 'Anchovas',
+        Image: '',
+        price: 40.29,
+    }
+]
